cc34af5ad4aa1e03b37b18ce16bebd02bda03136
Javon Davis <javonldavis14@gmail.com>
2016-09-16 01:40:05 -0500

    Added activity files for barcode detection and text detection

--- app/src/main/java/com/javon/playservicesdemo/textdetection/TextDetectionActivity.java
+++ app/src/main/java/com/javon/playservicesdemo/textdetection/TextDetectionActivity.java
@@ -7,6 +7,8 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -14,11 +16,14 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 import com.javon.playservicesdemo.R;
 
@@ -32,6 +37,7 @@ public class TextDetectionActivity extends AppCompatActivity {
     // Permission request codes need to be < 256
     private static final int RC_HANDLE_CAMERA_PERM = 2;
     private RelativeLayout container;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,22 +46,17 @@ public class TextDetectionActivity extends AppCompatActivity {
 
         container = (RelativeLayout) findViewById(R.id.activity_text_detection);
 
-        // Set good defaults for capturing text.
-        boolean autoFocus = true;
-        boolean useFlash = false;
+    }
 
+    public void takePicture(View view) {
         // Check for the camera permission before accessing the camera.  If the
         // permission is not granted yet, request permission.
         int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
         if (rc == PackageManager.PERMISSION_GRANTED) {
-            // Do something
+            dispatchTakePictureIntent();
         } else {
             requestCameraPermission();
         }
-    }
-
-    public void takePicture(View view) {
-
     }
 
     private void requestCameraPermission() {
@@ -85,6 +86,13 @@ public class TextDetectionActivity extends AppCompatActivity {
                 .show();
     }
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode != RC_HANDLE_CAMERA_PERM) {
@@ -94,8 +102,8 @@ public class TextDetectionActivity extends AppCompatActivity {
         }
 
         if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            Log.d(LOG_TAG, "Camera permission granted - initialize the camera source");
-
+            Log.d(LOG_TAG, "Camera permission granted, fire off the intent");
+            dispatchTakePictureIntent();
             return;
         }
 
@@ -109,9 +117,74 @@ public class TextDetectionActivity extends AppCompatActivity {
         };
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Multitracker sample")
+        builder.setTitle("Text Detection sample")
                 .setMessage("No camera")
                 .setPositiveButton("OK", listener)
                 .show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            String result = "Error retrieving image";
+
+            if(imageBitmap != null) {
+                StringBuilder detectedText = new StringBuilder();
+
+                TextRecognizer textRecognizer = new TextRecognizer.Builder(this).build();
+
+                if(!textRecognizer.isOperational()) {
+                    // Note: The first time that an app using a Vision API is installed on a
+                    // device, GMS will download a native libraries to the device in order to do detection.
+                    // Usually this completes before the app is run for the first time.  But if that
+                    // download has not yet completed, then the above call will not detect any text,
+                    // barcodes, or faces.
+                    //
+                    // isOperational() can be used to check if the required native libraries are currently
+                    // available.  The detectors will automatically become operational once the library
+                    // downloads complete on device.
+                    Log.w(LOG_TAG, "Detector dependencies are not yet available.");
+
+                    // Check for low storage.  If there is low storage, the native library will not be
+                    // downloaded, so detection will not become operational.
+                    IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
+                    boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
+
+                    if (hasLowStorage) {
+                        Toast.makeText(this,"Low Storage", Toast.LENGTH_LONG).show();
+                        Log.w(LOG_TAG, "Low Storage");
+                    }
+                }
+
+
+                Frame imageFrame = new Frame.Builder()
+                        .setBitmap(imageBitmap)
+                        .build();
+
+                SparseArray<TextBlock> textBlocks = textRecognizer.detect(imageFrame);
+
+                for (int i = 0; i < textBlocks.size(); i++) {
+                    TextBlock textBlock = textBlocks.get(i);
+
+                    detectedText.append(textBlock.getValue());
+                    detectedText.append("\n");
+                }
+
+
+                if ((result = detectedText.toString()).isEmpty()) {
+                    result = "Detected no text!";
+                }
+
+            }
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Text Detection sample")
+                    .setMessage(result)
+                    .setPositiveButton("OK", null)
+                    .show();
+
+        }
     }
 }
