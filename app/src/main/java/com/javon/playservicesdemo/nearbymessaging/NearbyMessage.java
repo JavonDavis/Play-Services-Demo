6a33ea51337d4ee0802d11d6ec6dd806fac0ea31
Javon Davis <javonldavis14@gmail.com>
2017-11-26 22:35:48 -0500

    Added steps to NearbyMessage class

--- app/src/main/java/com/javon/playservicesdemo/nearbymessaging/NearbyMessage.java
+++ app/src/main/java/com/javon/playservicesdemo/nearbymessaging/NearbyMessage.java
@@ -1,8 +1,31 @@
 package com.javon.playservicesdemo.nearbymessaging;
 
+import android.os.Build;
+
+import com.google.android.gms.nearby.messages.Message;
+import com.google.gson.Gson;
+
+import java.nio.charset.Charset;
+
 /**
  * Created by Javon Davis on 11/26/17.
  */
 
 public class NearbyMessage {
+
+    private static final Gson gson = new Gson();
+
+    // Step 9: Define body of message
+
+
+    // Step 10: Define constructor to set the body to some text
+
+
+    // Step 11: Create Getter for body
+
+    // Step 12: Create function to parse a Message and return a NearbyMessage
+
+
+    // Step 13: Create a function to return a Message from our NearbyMessage
+
 }
