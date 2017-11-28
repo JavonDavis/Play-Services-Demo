16f2a412d39794c6012f262156f37663fe34a44e
Javon Davis <javonldavis14@gmail.com>
2017-11-27 20:57:34 -0500

    Added steps

--- app/src/main/java/com/javon/playservicesdemo/textdetection/TextDetectionActivity.java
+++ app/src/main/java/com/javon/playservicesdemo/textdetection/TextDetectionActivity.java
@@ -1,20 +1,15 @@
 package com.javon.playservicesdemo.textdetection;
 
 import android.Manifest;
-import android.content.ContentResolver;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Matrix;
-import android.graphics.Paint;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
@@ -23,18 +18,11 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
-import android.util.SparseArray;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-import com.google.android.gms.common.images.ImageManager;
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.text.TextBlock;
-import com.google.android.gms.vision.text.TextRecognizer;
 import com.javon.playservicesdemo.R;
 
 import java.io.File;
@@ -63,6 +51,23 @@ public class TextDetectionActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageView);
     }
 
+    private void logAndShowSnackbar(final String text) {
+        Log.w(LOG_TAG, text);
+        View container = findViewById(R.id.activity_text_detection);
+        if (container != null) {
+            Snackbar.make(container, text, Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    private void requestPermissions() {
+        logAndShowSnackbar("Camera and Write permission is not granted. Requesting permission");
+
+        final String[] permissions = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        ActivityCompat.requestPermissions(this, permissions,
+                RC_HANDLE_PERMISSIONS);
+    }
+
     public void takePicture(View view) {
         // Check for the camera permission before accessing the camera.  If the
         // permission is not granted yet, request permission.
@@ -77,22 +82,6 @@ public class TextDetectionActivity extends AppCompatActivity {
         }
     }
 
-    private void requestPermissions() {
-        logAndShowSnackbar("Camera and Write permission is not granted. Requesting permission");
-
-        final String[] permissions = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-
-        ActivityCompat.requestPermissions(this, permissions,
-                RC_HANDLE_PERMISSIONS);
-    }
-
-    private void logAndShowSnackbar(final String text) {
-        Log.w(LOG_TAG, text);
-        View container = findViewById(R.id.activity_text_detection);
-        if (container != null) {
-            Snackbar.make(container, text, Snackbar.LENGTH_LONG).show();
-        }
-    }
 
     private File createImageFile() throws IOException {
         // Create an image file name
@@ -118,6 +107,8 @@ public class TextDetectionActivity extends AppCompatActivity {
                 photoFile = createImageFile();
             } catch (IOException ex) {
                 // Error occurred while creating the File
+                logAndShowSnackbar("Error occurred while creating the File");
+                return;
 
             }
             // Continue only if the File was successfully created
@@ -138,32 +129,18 @@ public class TextDetectionActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode != RC_HANDLE_PERMISSIONS) {
-            Log.d(LOG_TAG, "Got unexpected permission result: " + requestCode);
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-            return;
-        }
-
-        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
-                grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-            logAndShowSnackbar("Permissions granted, fire off the intent");
-            dispatchTakePictureIntent();
-            return;
-        }
-
-        logAndShowSnackbar("Permission not granted: results len = " + grantResults.length +
-                " Result code = " + (grantResults.length > 0 ? grantResults[0] : "(empty)"));
-    }
-
-    public Bitmap readBitmap(Uri selectedImage) {
+    /**
+     * Read in a bitmap from a URI and scale it down to avoid Out of Memory erros
+     * @param imageURI
+     * @return
+     */
+    public Bitmap readBitmap(Uri imageURI) {
         Bitmap bm = null;
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inSampleSize = 5;
         AssetFileDescriptor fileDescriptor =null;
         try {
-            fileDescriptor = this.getContentResolver().openAssetFileDescriptor(selectedImage,"r");
+            fileDescriptor = this.getContentResolver().openAssetFileDescriptor(imageURI,"r");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -178,6 +155,12 @@ public class TextDetectionActivity extends AppCompatActivity {
         return bm;
     }
 
+    /**
+     * Image from the camera comes it at -90 degree angle, we'd like to rotate it to get it upright
+     * @param source
+     * @param angle
+     * @return
+     */
     public static Bitmap rotateImage(Bitmap source, float angle) {
         Matrix matrix = new Matrix();
         matrix.postRotate(angle);
@@ -185,63 +168,23 @@ public class TextDetectionActivity extends AppCompatActivity {
                 matrix, true);
     }
 
-
-    private void detectText(Bitmap imageBitmap) {
-
-        StringBuilder detectedText = new StringBuilder();
-
-        TextRecognizer textRecognizer = new TextRecognizer.Builder(this).build();
-
-        if(!textRecognizer.isOperational()) {
-            // Note: The first time that an app using a Vision API is installed on a
-            // device, GMS will download a native libraries to the device in order to do detection.
-            // Usually this completes before the app is run for the first time.  But if that
-            // download has not yet completed, then the above call will not detect any text,
-            // barcodes, or faces.
-            //
-            // isOperational() can be used to check if the required native libraries are currently
-            // available.  The detectors will automatically become operational once the library
-            // downloads complete on device.
-            Log.w(LOG_TAG, "Detector dependencies are not yet available.");
-
-            // Check for low storage.  If there is low storage, the native library will not be
-            // downloaded, so detection will not become operational.
-            IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
-            boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
-
-            if (hasLowStorage) {
-                Toast.makeText(this,"Low Storage", Toast.LENGTH_LONG).show();
-                Log.w(LOG_TAG, "Low Storage");
-            }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode != RC_HANDLE_PERMISSIONS) {
+            logAndShowSnackbar("Got unexpected permission result: " + requestCode);
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            return;
         }
 
-
-        imageView.setImageBitmap(imageBitmap);
-
-        Frame imageFrame = new Frame.Builder()
-                .setBitmap(imageBitmap)
-                .build();
-
-        SparseArray<TextBlock> textBlocks = textRecognizer.detect(imageFrame);
-
-        for (int i = 0; i < textBlocks.size(); i++) {
-            TextBlock textBlock = textBlocks.get(i);
-
-            detectedText.append(textBlock.getValue());
-            detectedText.append("\n");
+        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+            logAndShowSnackbar("Permissions granted, fire off the intent");
+            dispatchTakePictureIntent();
+            return;
         }
 
-        String result = detectedText.toString();
-
-        if (result.isEmpty()) {
-            result = "Detected no text!";
-        }
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Text Detection sample")
-                .setMessage(result)
-                .setPositiveButton("OK", null)
-                .show();
+        logAndShowSnackbar("Permission not granted: results len = " + grantResults.length +
+                " Result code = " + (grantResults.length > 0 ? grantResults[0] : "(empty)"));
     }
 
     @Override
@@ -274,5 +217,13 @@ public class TextDetectionActivity extends AppCompatActivity {
                     .show();
 
         }
+    }
+
+    /**
+     * Process the image and detect text
+     * @param imageBitmap
+     */
+    private void detectText(Bitmap imageBitmap) {
+        // Big Step 3: Detect Text!
     }
 }
