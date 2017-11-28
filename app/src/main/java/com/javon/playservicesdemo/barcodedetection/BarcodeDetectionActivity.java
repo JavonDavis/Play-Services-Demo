88e51d72b8f1ca8c9526de6e17af0866c05f9f7e
Javon Davis <javonldavis14@gmail.com>
2017-11-27 22:46:22 -0500

    Cleaned up a lot of unnecessary code and tested to ensure barcodes are being detected

--- app/src/main/java/com/javon/playservicesdemo/barcodedetection/BarcodeDetectionActivity.java
+++ app/src/main/java/com/javon/playservicesdemo/barcodedetection/BarcodeDetectionActivity.java
@@ -7,7 +7,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -15,6 +19,7 @@ import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -29,14 +34,18 @@ import com.google.android.gms.vision.barcode.BarcodeDetector;
 import com.javon.playservicesdemo.R;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 public class BarcodeDetectionActivity extends AppCompatActivity {
 
     private static String LOG_TAG = "BarcodeDetection";
 
     // Permission request codes need to be < 256
-    private static final int RC_HANDLE_CAMERA_PERM = 2;
-    private RelativeLayout container;
+    private static final int RC_HANDLE_PERMISSIONS = 2;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private Uri mImageUri;
@@ -45,177 +54,214 @@ public class BarcodeDetectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barcode_detection);
+    }
 
-        container = (RelativeLayout) findViewById(R.id.activity_barcode_detection);
+    private void logAndShowSnackbar(final String text) {
+        Log.w(LOG_TAG, text);
+        View container = findViewById(R.id.activity_barcode_detection);
+        if (container != null) {
+            Snackbar.make(container, text, Snackbar.LENGTH_LONG).show();
+        }
+    }
 
+    private void requestPermissions() {
+        logAndShowSnackbar("Camera and Write permission is not granted. Requesting permission");
+
+        final String[] permissions = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        ActivityCompat.requestPermissions(this, permissions,
+                RC_HANDLE_PERMISSIONS);
     }
 
     public void takePicture(View view) {
         // Check for the camera permission before accessing the camera.  If the
         // permission is not granted yet, request permission.
-        int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
-        if (rc == PackageManager.PERMISSION_GRANTED) {
+        int cameraPermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        int writePermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (cameraPermission == PackageManager.PERMISSION_GRANTED && writePermission == PackageManager.PERMISSION_GRANTED) {
+            logAndShowSnackbar("Permissions granted");
             dispatchTakePictureIntent();
         } else {
-            requestCameraPermission();
+            requestPermissions();
         }
     }
 
-    private void requestCameraPermission() {
-        Log.w(LOG_TAG, "Camera permission is not granted. Requesting permission");
 
-        final String[] permissions = new String[]{Manifest.permission.CAMERA};
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
 
-        if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
-                Manifest.permission.CAMERA)) {
-            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
-            return;
-        }
-
-        final Activity thisActivity = this;
-
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ActivityCompat.requestPermissions(thisActivity, permissions,
-                        RC_HANDLE_CAMERA_PERM);
-            }
-        };
-
-        Snackbar.make(container, "Need camera.",
-                Snackbar.LENGTH_INDEFINITE)
-                .setAction("Ok", listener)
-                .show();
-    }
-
-    private File createTemporaryFile(String part, String ext) throws Exception
-    {
-        File tempDir= Environment.getExternalStorageDirectory();
-        tempDir=new File(tempDir.getAbsolutePath()+"/.temp/");
-        if(!tempDir.exists())
-        {
-            tempDir.mkdirs();
-        }
-        return File.createTempFile(part, ext, tempDir);
+        return image;
     }
 
     private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                logAndShowSnackbar("Error occurred while creating the File");
+                return;
 
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        File photo;
-        try
-        {
-            // place where to store camera taken picture
-            photo = createTemporaryFile("picture", ".jpg");
-            photo.delete();
+            }
+            // Continue only if the File was successfully created
+
+            if (photoFile != null) {
+                mImageUri = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                List<ResolveInfo> resInfoList = getPackageManager().queryIntentActivities(takePictureIntent, PackageManager.MATCH_DEFAULT_ONLY);
+                for (ResolveInfo resolveInfo : resInfoList) {
+                    String packageName = resolveInfo.activityInfo.packageName;
+                    grantUriPermission(packageName, mImageUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                }
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
+                takePictureIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+            }
         }
-        catch(Exception e)
-        {
-            Log.v(LOG_TAG, "Can't create file to take picture!");
-            Toast.makeText(this, "Please check SD card! Image shot is impossible!", Toast.LENGTH_LONG).show();
-            return ;
+    }
+
+    /**
+     * Read in a bitmap from a URI and scale it down to avoid Out of Memory erros
+     * @param imageURI
+     * @return
+     */
+    public Bitmap readBitmap(Uri imageURI) {
+        Bitmap bm = null;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 5;
+        AssetFileDescriptor fileDescriptor =null;
+        try {
+            fileDescriptor = this.getContentResolver().openAssetFileDescriptor(imageURI,"r");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        mImageUri = Uri.fromFile(photo);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        finally{
+            try {
+                bm = BitmapFactory.decodeFileDescriptor(fileDescriptor.getFileDescriptor(), null, options);
+                fileDescriptor.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+        return bm;
+    }
+
+    /**
+     * Image from the camera comes it at -90 degree angle, we'd like to rotate it to get it upright
+     * @param source
+     * @param angle
+     * @return
+     */
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
+
+
+    public void detectBarcode(Bitmap imageBitmap) {
+        StringBuilder barcodeInfo = new StringBuilder();
+
+        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+
+        if(!barcodeDetector.isOperational()) {
+            // Note: The first time that an app using a Vision API is installed on a
+            // device, GMS will download a native libraries to the device in order to do detection.
+            // Usually this completes before the app is run for the first time.  But if that
+            // download has not yet completed, then the above call will not detect any text,
+            // barcodes, or faces.
+            //
+            // isOperational() can be used to check if the required native libraries are currently
+            // available.  The detectors will automatically become operational once the library
+            // downloads complete on device.
+            Log.w(LOG_TAG, "Detector dependencies are not yet available.");
+
+            // Check for low storage.  If there is low storage, the native library will not be
+            // downloaded, so detection will not become operational.
+            IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
+            boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
+
+            if (hasLowStorage) {
+                Toast.makeText(this,"Low Storage", Toast.LENGTH_LONG).show();
+                Log.w(LOG_TAG, "Low Storage");
+            }
+        }
+
+
+        Frame imageFrame = new Frame.Builder()
+                .setBitmap(imageBitmap)
+                .build();
+
+        SparseArray<Barcode> barcodes = barcodeDetector.detect(imageFrame);
+
+        for (int i = 0; i < barcodes.size(); i++) {
+            Barcode barcode = barcodes.get(barcodes.keyAt(i));
+
+            barcodeInfo.append(barcode.displayValue);
+            barcodeInfo.append("\n");
+        }
+
+        String result = barcodeInfo.toString();
+
+        if (result.isEmpty()) {
+            result = "Detected no Barcode!";
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Barcode Detection sample")
+                .setMessage(result)
+                .setPositiveButton("OK", null)
+                .show();
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode != RC_HANDLE_CAMERA_PERM) {
-            Log.d(LOG_TAG, "Got unexpected permission result: " + requestCode);
+        if (requestCode != RC_HANDLE_PERMISSIONS) {
+            logAndShowSnackbar("Got unexpected permission result: " + requestCode);
             super.onRequestPermissionsResult(requestCode, permissions, grantResults);
             return;
         }
 
-        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            Log.d(LOG_TAG, "Camera permission granted, fire off the intent");
+        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+            logAndShowSnackbar("Permissions granted, fire off the intent");
             dispatchTakePictureIntent();
             return;
         }
 
-        Log.e(LOG_TAG, "Permission not granted: results len = " + grantResults.length +
+        logAndShowSnackbar("Permission not granted: results len = " + grantResults.length +
                 " Result code = " + (grantResults.length > 0 ? grantResults[0] : "(empty)"));
-
-        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                finish();
-            }
-        };
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Barcode Detection sample")
-                .setMessage("No camera")
-                .setPositiveButton("OK", listener)
-                .show();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-//            Bundle extras = data.getExtras();
-            //Bitmap imageBitmap = (Bitmap) extras.get("data");
-            String result = "Error retrieving image";
-
             this.getContentResolver().notifyChange(mImageUri, null);
-            ContentResolver cr = this.getContentResolver();
             Bitmap imageBitmap;
             try
             {
-                imageBitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, mImageUri);
+                imageBitmap = readBitmap(mImageUri);
+                Bitmap rotatedBitmap = rotateImage(imageBitmap, 90);
 
                 if(imageBitmap != null) {
-                    StringBuilder barcodeInfo = new StringBuilder();
-
-                    BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this)
-                            .setBarcodeFormats(Barcode.ALL_FORMATS)
-                            .build();
-
-                    if(!barcodeDetector.isOperational()) {
-                        // Note: The first time that an app using a Vision API is installed on a
-                        // device, GMS will download a native libraries to the device in order to do detection.
-                        // Usually this completes before the app is run for the first time.  But if that
-                        // download has not yet completed, then the above call will not detect any text,
-                        // barcodes, or faces.
-                        //
-                        // isOperational() can be used to check if the required native libraries are currently
-                        // available.  The detectors will automatically become operational once the library
-                        // downloads complete on device.
-                        Log.w(LOG_TAG, "Detector dependencies are not yet available.");
-
-                        // Check for low storage.  If there is low storage, the native library will not be
-                        // downloaded, so detection will not become operational.
-                        IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
-                        boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
-
-                        if (hasLowStorage) {
-                            Toast.makeText(this,"Low Storage", Toast.LENGTH_LONG).show();
-                            Log.w(LOG_TAG, "Low Storage");
-                        }
-                    }
-
-
-                    Frame imageFrame = new Frame.Builder()
-                            .setBitmap(imageBitmap)
-                            .build();
-
-                    SparseArray<Barcode> barcodes = barcodeDetector.detect(imageFrame);
-
-                    for (int i = 0; i < barcodes.size(); i++) {
-                        Barcode barcode = barcodes.get(barcodes.keyAt(i));
-
-                        barcodeInfo.append(barcode.displayValue);
-                        barcodeInfo.append("\n");
-                    }
-
-                    result = barcodeInfo.toString();
-
-                    if (result.isEmpty()) {
-                        result = "Detected no Barcode!";
-                    }
-
+                    detectBarcode(rotatedBitmap);
                 }
             }
             catch (Exception e)
@@ -223,13 +269,6 @@ public class BarcodeDetectionActivity extends AppCompatActivity {
                 Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT).show();
                 Log.d(LOG_TAG, "Failed to load", e);
             }
-
-
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Barcode Detection sample")
-                    .setMessage(result)
-                    .setPositiveButton("OK", null)
-                    .show();
 
         }
     }
