4ca8110912f9eb3be2cdebd24425c817b6c40b37
Javon Davis <javonldavis14@gmail.com>
2017-11-27 20:16:33 -0500

    Fixed out of memory error that was sometimes cause by image size and also changed location of file.

--- app/src/main/java/com/javon/playservicesdemo/textdetection/TextDetectionActivity.java
+++ app/src/main/java/com/javon/playservicesdemo/textdetection/TextDetectionActivity.java
@@ -1,161 +1,196 @@
 package com.javon.playservicesdemo.textdetection;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.ContentResolver;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.common.images.ImageManager;
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 import com.javon.playservicesdemo.R;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 public class TextDetectionActivity extends AppCompatActivity {
 
     private static String LOG_TAG = "TextDetectionActivity";
 
-    // Intent request code to handle updating play services if needed.
-    private static final int RC_HANDLE_GMS = 9001;
-
     // Permission request codes need to be < 256
-    private static final int RC_HANDLE_CAMERA_PERM = 2;
-    private RelativeLayout container;
+    private static final int RC_HANDLE_PERMISSIONS = 2;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private Uri mImageUri;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_detection);
 
-        container = (RelativeLayout) findViewById(R.id.activity_text_detection);
+        imageView = (ImageView) findViewById(R.id.imageView);
 
     }
 
     public void takePicture(View view) {
         // Check for the camera permission before accessing the camera.  If the
         // permission is not granted yet, request permission.
-        int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
-        if (rc == PackageManager.PERMISSION_GRANTED) {
+        int cameraPermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        int writePermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (cameraPermission == PackageManager.PERMISSION_GRANTED && writePermission == PackageManager.PERMISSION_GRANTED) {
+            logAndShowSnackbar("Permissions granted");
             dispatchTakePictureIntent();
         } else {
-            requestCameraPermission();
+            requestPermissions();
         }
     }
 
-    private void requestCameraPermission() {
-        Log.w(LOG_TAG, "Camera permission is not granted. Requesting permission");
+    private void requestPermissions() {
+        logAndShowSnackbar("Camera and Write permission is not granted. Requesting permission");
 
-        final String[] permissions = new String[]{Manifest.permission.CAMERA};
+        final String[] permissions = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
-        if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
-                Manifest.permission.CAMERA)) {
-            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
-            return;
-        }
-
-        final Activity thisActivity = this;
-
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ActivityCompat.requestPermissions(thisActivity, permissions,
-                        RC_HANDLE_CAMERA_PERM);
-            }
-        };
-
-        Snackbar.make(container, "Need camera.",
-                Snackbar.LENGTH_INDEFINITE)
-                .setAction("Ok", listener)
-                .show();
+        ActivityCompat.requestPermissions(this, permissions,
+                RC_HANDLE_PERMISSIONS);
     }
 
-    private File createTemporaryFile(String part, String ext) throws Exception
-    {
-        File tempDir= Environment.getExternalStorageDirectory();
-        tempDir=new File(tempDir.getAbsolutePath()+"/.temp/");
-        if(!tempDir.exists())
-        {
-            tempDir.mkdirs();
+    private void logAndShowSnackbar(final String text) {
+        Log.w(LOG_TAG, text);
+        View container = findViewById(R.id.activity_text_detection);
+        if (container != null) {
+            Snackbar.make(container, text, Snackbar.LENGTH_LONG).show();
         }
-        return File.createTempFile(part, ext, tempDir);
+    }
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
     private void dispatchTakePictureIntent() {
 
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        File photo;
-        try
-        {
-            // place where to store camera taken picture
-            photo = createTemporaryFile("picture", ".jpg");
-            photo.delete();
-        }
-        catch(Exception e)
-        {
-            Log.v(LOG_TAG, "Can't create file to take picture!");
-            Toast.makeText(this, "Please check SD card! Image shot is impossible!", Toast.LENGTH_LONG).show();
-            return ;
-        }
-        mImageUri = Uri.fromFile(photo);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+
+            if (photoFile != null) {
+                mImageUri = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                List<ResolveInfo> resInfoList = getPackageManager().queryIntentActivities(takePictureIntent, PackageManager.MATCH_DEFAULT_ONLY);
+                for (ResolveInfo resolveInfo : resInfoList) {
+                    String packageName = resolveInfo.activityInfo.packageName;
+                    grantUriPermission(packageName, mImageUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                }
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
+                takePictureIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+            }
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode != RC_HANDLE_CAMERA_PERM) {
+        if (requestCode != RC_HANDLE_PERMISSIONS) {
             Log.d(LOG_TAG, "Got unexpected permission result: " + requestCode);
             super.onRequestPermissionsResult(requestCode, permissions, grantResults);
             return;
         }
 
-        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            Log.d(LOG_TAG, "Camera permission granted, fire off the intent");
+        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+            logAndShowSnackbar("Permissions granted, fire off the intent");
             dispatchTakePictureIntent();
             return;
         }
 
-        Log.e(LOG_TAG, "Permission not granted: results len = " + grantResults.length +
+        logAndShowSnackbar("Permission not granted: results len = " + grantResults.length +
                 " Result code = " + (grantResults.length > 0 ? grantResults[0] : "(empty)"));
-
-        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                finish();
-            }
-        };
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Text Detection sample")
-                .setMessage("No camera")
-                .setPositiveButton("OK", listener)
-                .show();
     }
+
+    public Bitmap readBitmap(Uri selectedImage) {
+        Bitmap bm = null;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 5;
+        AssetFileDescriptor fileDescriptor =null;
+        try {
+            fileDescriptor = this.getContentResolver().openAssetFileDescriptor(selectedImage,"r");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        finally{
+            try {
+                bm = BitmapFactory.decodeFileDescriptor(fileDescriptor.getFileDescriptor(), null, options);
+                fileDescriptor.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return bm;
+    }
+
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -169,7 +204,7 @@ public class TextDetectionActivity extends AppCompatActivity {
             Bitmap imageBitmap;
             try
             {
-                imageBitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, mImageUri);
+                imageBitmap = readBitmap(mImageUri);
 
                 if(imageBitmap != null) {
                     StringBuilder detectedText = new StringBuilder();
@@ -199,9 +234,11 @@ public class TextDetectionActivity extends AppCompatActivity {
                         }
                     }
 
+                    Bitmap rotatedBitmap = rotateImage(imageBitmap, 90);
+                    imageView.setImageBitmap(rotatedBitmap);
 
                     Frame imageFrame = new Frame.Builder()
-                            .setBitmap(imageBitmap)
+                            .setBitmap(rotatedBitmap)
                             .build();
 
                     SparseArray<TextBlock> textBlocks = textRecognizer.detect(imageFrame);
